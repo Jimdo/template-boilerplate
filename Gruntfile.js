111b4452454dd38fd89620b47b65e249a24228f2
filtercake <fabian@superpursuitmo.de>
2014-07-23 07:07:35 +0200

    reset csslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,18 +3,7 @@ module.exports = function(grunt){
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-
-    // COMPILE TASKS
-    // coffee: {
-    //   glob_to_multiple: {
-    //     expand: true,
-    //     // flatten: true,
-    //     cwd: 'src/',
-    //     src: ['**/*.coffee'],
-    //     dest: 'dev',
-    //     ext: '.js',
-    //   },
-    // },
+    // CSS
     sass: {
       dist: {
         options: {
@@ -29,65 +18,30 @@ module.exports = function(grunt){
           expand: true,
           cwd: 'src',
           src: ['**/*.sass'],
-          dest: 'dev',
+          dest: 'css',
           ext: '.css'
         }],
       }
     },
-    // jade: {
-    //   compile: {
-    //     options: {
-    //       pretty: true
-    //     },
-    //     files: [ {
-    //       cwd: 'src',
-    //       dest: 'dev',
-    //       expand: true,
-    //       src: '**/*.jade',
-    //       ext: '.html',
-    //     } ]
-    //   }
-    // },
 
-    // COPY TASKS
-    // font awesomesome example in commit fc820d0
-    // copy: {
-
-    //   bower_components: {
-    //   // bower components need to be available
-    //   // TODO: instead set install path to dev with .bowerrc
-    //     expand: true,
-    //     cwd: 'bower_components',
-    //     dest: 'dev/bower_components',
-    //     src: '**/*',
-    //   }
-
-    // },
-
+    csslint: {
+      kompilat: {
+        options: {
+          csslintrc: '.csslintrc'
+        },
+        src: [
+          'css/**/*.css'
+          ]
+      }
+    },
 
     // WATCH
     watch: {
-
-      // html: {
-      //   files: ['src/**/*.html'],
-      //   tasks: ['copy:html'],
-      // },
-
-      // jade: {
-      //   files: ['src/**/*.jade'],
-      //   tasks: ['jade'],
-      // },
-
-      // coffee: {
-      //   files: [ 'src/**/*.coffee' ],
-      //   tasks: [ 'coffee' ],
-      // },
 
       sass: {
         files: 'src/**/*.sass',
         tasks: [
           'sass',
-          // 'csscomb:sass',
           'csslint:kompilat'
           ],
         options: {
@@ -95,15 +49,6 @@ module.exports = function(grunt){
         }
       },
 
-      // watch_build: {
-      //   files: [
-      //     'dev/**/*.html',
-      //     'dev/**/*.js'
-      //   ],
-      //   options: {
-      //     livereload: true,
-      //   },
-      // },
       watch_build_css: {
         files: [
           'css/**/*.css'
@@ -114,6 +59,7 @@ module.exports = function(grunt){
       }
     },
 
+    // SERVER
     connect: {
       server: {
         options: {
@@ -124,48 +70,16 @@ module.exports = function(grunt){
       }
     },
 
-
     // CONCURRENT FOR WATCH AND SERVE
     concurrent: {
-        watch_serve_reload: ['server', 'watch'],
-        options: {
-                logConcurrentOutput: true
-            }
-    },
-
-    // csscomb: {
-    //     sass: {
-    //         expand: true,
-    //         cwd: 'src',
-    //         src: ['**/*.sass'],
-    //         dest: 'src',
-    //         ext: '.sass'
-    //     }
-    // },
-
-    csslint: {
-      kompilat: {
-        options: {
-          csslintrc: '.csslintrc'
-        },
-        src: [
-          'css/**/*.css'
-          // '!dev/bower_components/**/*.css'
-          ]
+      watch_serve_reload: ['server', 'watch'],
+      options: {
+        logConcurrentOutput: true
       }
     }
-
   });
 
-  grunt.registerTask(
-    'inital_compile', [
-       'sass'
-       // 'jade',
-       // 'coffee',
-       // 'copy'
-        ]
-  );
-
+  grunt.registerTask('inital_compile', [ 'sass' ]);
   grunt.registerTask('server', [ 'connect:server:keepalive' ]);
   grunt.registerTask('default', [ 'inital_compile', 'concurrent:watch_serve_reload' ]);
 
@@ -173,10 +87,5 @@ module.exports = function(grunt){
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-concurrent');
-  // grunt.loadNpmTasks('grunt-contrib-jade');
-  // grunt.loadNpmTasks('grunt-contrib-copy');
-  // grunt.loadNpmTasks('grunt-contrib-jshint');
-  // grunt.loadNpmTasks('grunt-contrib-coffee');
-  // grunt.loadNpmTasks('grunt-csscomb');
   grunt.loadNpmTasks('grunt-contrib-csslint');
 };
